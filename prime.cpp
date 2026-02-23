ff0603987f57ff76ce56d185cbe9e5dfc974ff62
Ravi Ranjan <137782361+raviranjancom@users.noreply.github.com>
2026-02-23 22:12:50 +0530

    Refactor prime checking functions and add isPrime_
    Refactored prime checking logic and added isPrime_ function for improved efficiency and clarity.

--- prime.cpp
+++ prime.cpp
@@ -2,14 +2,36 @@
 using namespace std;
 bool isprime(int n){
     int count=0;
-    for(int i=2;i<pow(n, 0.5);i++){
+    for(int i=2;i<=pow(n, 0.5);i++){
         if(n%i==0) count++;
     }
     if(count>0) return true;
     return false;
 }
+bool isPrime_(int n){
+    if(n<2) return false;
+    if(n==2 || n==3 || n==5){
+        cout<<"Yes!\n";
+        return true;
+    }
+    if(n%2==0 || n%3==0 || n%5==0){
+        cout<<"No!\n";
+        return false;
+    }
+    for(int k=6;(k+1)*(k+1)<=n;k+=6){
+        int x=(k-1);
+        int y=(k+1);
 
+        if(n%x==0 || n%y==0){
+            cout<<"No!\n";
+            return false;
+        }
+    }
+    cout<<"Yes!\n";
+    return true;
+}
 int main(){
-    int n=100;
-    cout<<isprime(n);
+    int n=293;
+    isPrime_(n);
+    return 0;
 }
